760b9df6d8f46eeccf56f796b08f6f4f9c8e99e7
Mingli Yuan <mingli.yuan@gmail.com>
2012-11-29 23:21:23 +0800

    remove stanford-nlp java source

--- src/java/edu/stanford/nlp/trees/DependencyPrinter.java
+++
@@ -1,7 +0,0 @@
-package edu.stanford.nlp.trees;
-
-import java.util.Collection;
-
-public interface DependencyPrinter {
-    public String dependenciesToString(GrammaticalStructure gs, Collection<TypedDependency> deps, Tree tree);
-}
